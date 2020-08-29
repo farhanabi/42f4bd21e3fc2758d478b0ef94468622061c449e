92e3b0bf85c6f892a7b9729cf64d43aaca55fb77
Farhan Abi <farhanabiyullah@gmail.com>
2020-08-29 11:19:39 +0700

    Build

---
+++ build/precache-manifest.d693b00267cdb2da345e425d851e8792.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1abee8354aa3335f965fc9b3a0681d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91544952ff15f472e0c5",
+    "url": "/static/css/main.adeb9d33.chunk.css"
+  },
+  {
+    "revision": "25bbf740d9ec63d71244",
+    "url": "/static/js/2.b79cc45d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b79cc45d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91544952ff15f472e0c5",
+    "url": "/static/js/main.b849fef8.chunk.js"
+  },
+  {
+    "revision": "d9bca075a64d1a00b22b",
+    "url": "/static/js/runtime-main.2ec3893e.js"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "/static/media/Proxima Nova Bold.62d4d7d3.otf"
+  },
+  {
+    "revision": "076d851b602b9915c429f3a2e436c639",
+    "url": "/static/media/Proxima Nova Light.076d851b.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/static/media/Proxima Nova Regular.410504d4.otf"
+  },
+  {
+    "revision": "6a386899746222073dd64c5f74d1a69d",
+    "url": "/static/media/Proxima Nova Semibold.6a386899.otf"
+  },
+  {
+    "revision": "bc09fdce0a1279b925be66384ffd52ec",
+    "url": "/static/media/google.bc09fdce.png"
+  },
+  {
+    "revision": "ece730d7399c810cb19a012914caa960",
+    "url": "/static/media/menu-1-roasted-chicken.ece730d7.png"
+  },
+  {
+    "revision": "9563695c8b487512e5f8d970e493565b",
+    "url": "/static/media/menu-2-roasted-chicken.9563695c.png"
+  },
+  {
+    "revision": "377e46fe89acbf3620ea9e91d3720858",
+    "url": "/static/media/menu-3-roasted-chicken.377e46fe.png"
+  },
+  {
+    "revision": "902fc004d59e98b189d4844abf204655",
+    "url": "/static/media/menu-4-roasted-chicken.902fc004.png"
+  },
+  {
+    "revision": "65f5de6698ae3db0b43aefc5bc9d1203",
+    "url": "/static/media/menu-5-roasted-chicken.65f5de66.png"
+  },
+  {
+    "revision": "219817027c39e00c3a33b749fd0e35c4",
+    "url": "/static/media/menu-6-roasted-chicken.21981702.png"
+  }
+]);
